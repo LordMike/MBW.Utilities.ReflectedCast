ed67539984a16eefbd55a99008eb85c38912f46b
Michael Bisbjerg <michael@mbwarez.dk>
2018-07-29 17:24:27 +0200

    Expanded explicit implementation tests

--- ReflectedCast.Tests/ExplicitImplementationCases.cs
+++ ReflectedCast.Tests/ExplicitImplementationCases.cs
@@ -7,9 +7,10 @@ namespace ReflectedCast.Tests
         [Fact]
         public void ExplicitImplementation()
         {
+            // Tests a straightforward setup (same interfaces)
             TestClass orig = new TestClass();
-            ITestInterfaceFirst asInterfaceFirst = ReflectedCaster.CastToInterface<ITestInterfaceFirst>(orig);
-            ITestInterfaceSecond asInterfaceSecond = ReflectedCaster.CastToInterface<ITestInterfaceSecond>(orig);
+            FirstScope.ITestInterfaceFirst asInterfaceFirst = ReflectedCaster.CastToInterface<FirstScope.ITestInterfaceFirst>(orig);
+            FirstScope.ITestInterfaceSecond asInterfaceSecond = ReflectedCaster.CastToInterface<FirstScope.ITestInterfaceSecond>(orig);
 
             asInterfaceFirst.Method();
             Assert.Equal(1, orig.ReturnCode);
@@ -18,17 +19,22 @@ namespace ReflectedCast.Tests
             Assert.Equal(2, orig.ReturnCode);
         }
 
-        public interface ITestInterfaceFirst
+        [Fact]
+        public void ExplicitImplementationDifferentInterfaces()
         {
-            void Method();
+            // Tests the case where the interfaces are actually different, but the names are the same
+            TestClass orig = new TestClass();
+            OtherScope.ITestInterfaceFirst asInterfaceFirst = ReflectedCaster.CastToInterface<OtherScope.ITestInterfaceFirst>(orig);
+            OtherScope.ITestInterfaceSecond asInterfaceSecond = ReflectedCaster.CastToInterface< OtherScope.ITestInterfaceSecond> (orig);
+
+            asInterfaceFirst.Method();
+            Assert.Equal(1, orig.ReturnCode);
+
+            asInterfaceSecond.Method();
+            Assert.Equal(2, orig.ReturnCode);
         }
 
-        public interface ITestInterfaceSecond
-        {
-            void Method();
-        }
-
-        public class TestClass : ITestInterfaceFirst, ITestInterfaceSecond
+        public class TestClass : FirstScope.ITestInterfaceFirst, FirstScope.ITestInterfaceSecond
         {
             public object ReturnCode { get; set; }
 
@@ -37,14 +43,40 @@ namespace ReflectedCast.Tests
                 ReturnCode = 3;
             }
 
-            void ITestInterfaceFirst.Method()
+            void FirstScope.ITestInterfaceFirst.Method()
             {
                 ReturnCode = 1;
             }
 
-            void ITestInterfaceSecond.Method()
+            void FirstScope.ITestInterfaceSecond.Method()
             {
                 ReturnCode = 2;
+            }
+        }
+
+        public class FirstScope
+        {
+            public interface ITestInterfaceFirst
+            {
+                void Method();
+            }
+
+            public interface ITestInterfaceSecond
+            {
+                void Method();
+            }
+        }
+
+        public class OtherScope
+        {
+            public interface ITestInterfaceFirst
+            {
+                void Method();
+            }
+
+            public interface ITestInterfaceSecond
+            {
+                void Method();
             }
         }
     }
