9fa74e9f86fb4815904b61677abc8e0d8043557f
Michael Bisbjerg <michael@mbwarez.dk>
2018-07-29 17:20:07 +0200

    Add tests for explicit&inherited implementations

---
+++ ReflectedCast.Tests/ExplicitInheritedImplementationCases.cs
@@ -0,0 +1,54 @@
+﻿using Xunit;
+
+namespace ReflectedCast.Tests
+{
+    public class ExplicitInheritedImplementationCases
+    {
+        [Fact]
+        public void ExplicitInheritedImplementation()
+        {
+            TestClass orig = new TestClass();
+            ITestInterfaceFirst asInterfaceFirst = ReflectedCaster.CastToInterface<ITestInterfaceFirst>(orig);
+            ITestInterfaceSecond asInterfaceSecond = ReflectedCaster.CastToInterface<ITestInterfaceSecond>(orig);
+
+            asInterfaceFirst.Method();
+            Assert.Equal(1, orig.ReturnCode);
+
+            asInterfaceSecond.Method();
+            Assert.Equal(2, orig.ReturnCode);
+        }
+
+        public interface ITestInterfaceFirst
+        {
+            void Method();
+        }
+
+        public interface ITestInterfaceSecond
+        {
+            void Method();
+        }
+
+        public class TestBaseClass : ITestInterfaceFirst
+        {
+            public object ReturnCode { get; set; }
+
+            public void Method()
+            {
+                ReturnCode = 3;
+            }
+
+            void ITestInterfaceFirst.Method()
+            {
+                ReturnCode = 1;
+            }
+        }
+
+        public class TestClass : TestBaseClass, ITestInterfaceSecond
+        {
+            void ITestInterfaceSecond.Method()
+            {
+                ReturnCode = 2;
+            }
+        }
+    }
+}
